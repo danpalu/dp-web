54208b0ddbd39cd7c7c26b0a09af2849a86e2c82
dantjald <56084889+dantjald@users.noreply.github.com>
2024-01-17 17:38:25 +0100

    Custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://depe.dev",
-  base: "/dp-web",
+  site: "/",
+  base: "/",
 });
