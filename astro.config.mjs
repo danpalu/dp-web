4d823ff38bc557b462eec90329a9d2c06f498af9
dantjald <56084889+dantjald@users.noreply.github.com>
2024-01-17 17:42:57 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://danpalu.github.io/dp-web",
-  base: "/",
+  site: "https://danpalu.github.io/",
+  base: "/dp-web",
 });
